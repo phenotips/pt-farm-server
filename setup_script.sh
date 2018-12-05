6af5956dea98ceee4f8d8bb5dc061a073b03495e
Daniel Snider <danielsnider12@gmail.com>
2018-12-05 11:54:14 -0500

    More notes

--- setup_script.sh
+++ setup_script.sh
@@ -1,22 +1,49 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo yum install mc tomcat epel-release screen tmux mysql-server mod_ssl ntpdate ntp jetty rsync telnet vim
+sudo yum install -y mc tomcat epel-release screen tmux mysql-server mod_ssl ntpdate ntp jetty rsync telnet vim git
 
 curl https://phenotips.org/download/PhenoTips/Download/PhenoTips.repo > PhenoTips.repo
 sudo chown root:root PhenoTips.repo
 sudo mv PhenoTips.repo /etc/yum.repos.d/
-sudo yum install phenotips
+sudo yum install -y phenotips
+
+sudo sed '/^127\.0\.0\.1/ s/$/ test.ccm.sickkids.ca/' /etc/hosts
+
+sudo hostnamectl set-hostname webapps.ccm.sickkids.ca
+
+cd ~
+git clone https://github.com/phenotips/pt-farm-server
+cd pt-farm-server
+
+keybase pgp decrypt -i authorized_keys.asc -o ~/.ssh/authorized_keys
+
+sudo systemctl start ntpd
+sudo chkconfig ntpd on
+
+
+curl https://repo.mysql.com//mysql80-community-release-el7-1.noarch.rpm -o mysql80-community-release-el7-1.noarch.rpm
+sudo rpm -ivh mysql80-community-release-el7-1.noarch.rpm
+sudo yum update -y
+sudo yum install -y mysql-server
+rm mysql80-community-release-el7-1.noarch.rpm
+sudo systemctl start mysqld
+
+
+
+
+
+
+sudo grep 'temporary password' /var/log/mysqld.log | rev | cut -d' ' -f 1 | rev
+
+
 
 /etc/httpd/conf.d/phenotips.conf
 
 mv /etc/tomcat/Catalina/localhost/phenotips.xml /etc/tomcat/Catalina/localhost/ROOT.xml
 
- sudo systemctl start httpd
- sudo systemctl start tomcat
-
-
-
+sudo systemctl start httpd
+sudo systemctl start tomcat
 
 
 
@@ -24,7 +51,6 @@ mv /etc/tomcat/Catalina/localhost/phenotips.xml /etc/tomcat/Catalina/localhost/R
 
 /usr/bin/mysqld_safe --skip-grant-tables &
 vi /etc/resolv.conf
-vi /etc/ntp.conf 
 
 
 73  /etc/init.d/mysqld start
@@ -45,4 +71,6 @@ vi /etc/ntp.conf
 chkconfig tomcat on
 chkconfig mysqld on
 chkconfig httpd on
-chkconfig ntpd on
+
+
+# sudo reboot
